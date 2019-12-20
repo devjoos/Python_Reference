f2591d5e2e0053a2c9db53bf5d76a4cbddde5bed
Sam Joos <joosbox@Sams-MacBook-Air.local>
2019-12-20 13:13:30 -0700

    dec17MBABOD

--- purefunctions.py
+++ purefunctions.py
@@ -1,12 +1,35 @@
-map(multiply_by2)
+from functools import reduce
+
+my_list = [1, 2, 3, 4]
+your_list = [10, 20, 30, 40, 50, 50, 50]
+my_set = {1, 2, 3, 4, 5, 5, 5}
 
 
-def multply_by2(li):
-    new_list = []
-    for item in li:
-        new_list.append(item*2)
-    return new_list
+def multiply_by2(item):
+    return item * 2
+    # new_list = []
+    # for item in li:
+    #     new_list.append(item*2)
+    # return new_list
 
 
-new_list = 12
-print(multply_by2([1, 2, 3]))
+def only_odd(item):
+    return item % 2 != 0
+
+
+def accumulator(acc, item):
+    print(acc, item)
+    return acc + item
+
+
+print(list(map(multiply_by2, my_list)))
+odd_list = list(filter(only_odd, my_list))
+zip_list = list(zip(my_list, your_list, my_set))
+reduced = reduce(accumulator, my_list, 0)
+# print(my_list)
+# print(new_list)
+# print(odd_list)
+# print(zip_list)
+print(reduced)
+# new_list = 12
+# print(multiply_by2([1, 2, 3]))
